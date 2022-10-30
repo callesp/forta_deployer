818f05496a936a1922c531ff185496cd50fb5e2d
admin <you@example.com>
2022-10-31 00:20:45 +0800

    Update clean.sh.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# kill -9 `ps axu | grep inst_stage* | grep -v grep | awk '{print $2}'`
+# kill -9 `ps axu | grep clean.sh* | grep -v grep | awk '{print $2}'`
+
 systemctl stop forta
 
 systemctl disable forta
@@ -7,5 +10,7 @@ systemctl disable forta
 yum remove -y docker-ce docker-ce-cli containerd.io docker-compose-plugin forta
 
 rm -rf /etc/docker
+
+rm -rf /etc/systemd/system/forta.service.d
 
 userdel -r forta
